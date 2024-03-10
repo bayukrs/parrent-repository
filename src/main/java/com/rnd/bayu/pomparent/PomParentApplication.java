0cc1b06ae9f7a32f5e81981cc451a13007343417
bayu <bayukrismant@gmail.com>
2024-03-10 23:12:38 +0700

    initial commit

---
+++ src/main/java/com/rnd/bayu/pomparent/PomParentApplication.java
@@ -0,0 +1,13 @@
+package com.rnd.bayu.pomparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomParentApplication.class, args);
+	}
+
+}
